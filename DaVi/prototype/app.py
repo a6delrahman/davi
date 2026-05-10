7340314b5e13b5f35ef9015b972bfa10789e72df
a6delrahman <86350220+a6delrahman@users.noreply.github.com>
2026-05-10 16:31:13 +0200

    Update app.py

--- DaVi/prototype/app.py
+++ DaVi/prototype/app.py
@@ -12,7 +12,7 @@ st.set_page_config(
 # --- Phase 1: Data Loading ---
 @st.cache_data
 def load_data():
-    df = pd.read_csv('samsung_global_sales_dataset.csv')
+    df = pd.read_csv('DaVi/prototype/samsung_global_sales_dataset.csv')
     df['sale_date'] = pd.to_datetime(df['sale_date'])
     return df
 
